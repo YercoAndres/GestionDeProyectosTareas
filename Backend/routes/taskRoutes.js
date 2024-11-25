02113f2f06d1e7516661c4cdd03f5d50e4f0f0db
Yerco <yerco.andres.soto@gmail.com>
2024-11-25 16:06:43 -0300

    Librerias

--- Backend/routes/taskRoutes.js
+++ Backend/routes/taskRoutes.js
@@ -5,5 +5,6 @@ const taskController = require('../controllers/taskController');
 
 // Define la ruta para agregar una tarea a un proyecto
 router.post('/:projectId/tasks', taskController.createTask);
+router.get('/:projectId/tasks', taskController.getTasksByProjectId);
 
 module.exports = router;
