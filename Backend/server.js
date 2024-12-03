6413a989a708fe06bd620a629d7ef3a9bc3f1bc5
Yerco <yerco.andres.soto@gmail.com>
2024-12-03 13:42:39 -0300

    update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -14,20 +14,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-app.get('/', (req, res) => {
-    res.send('API is running...');
-}   );
-
 app.use('/api/auth', authRoutes);
-app.use('/api/projects', projectRoutes);
-app.use('/api/tasks', taskRoutes);
-app.use('/api/users', userRoutes);
-
-// Error 404 para rutas no encontradas
-app.use((req, res) => {
-    res.status(404).json({ error: 'Ruta no encontrada' });
-});
+app.use('/projects', projectRoutes);
+app.use('/tasks', taskRoutes); 
+app.use('/api/users', userRoutes); 
 
 app.use(errorHandler);
 
