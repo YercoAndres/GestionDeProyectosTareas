28003f35834bc9a500306c82dcc3cc4e72987117
Yerco <yerco.andres.soto@gmail.com>
2024-12-04 10:26:36 -0300

    update login

--- Frontend/gestion_proyectos_tareas/src/pages/Login.jsx
+++ Frontend/gestion_proyectos_tareas/src/pages/Login.jsx
@@ -41,11 +41,13 @@ function Login() {
       toast.error(error.message);
     }
   };
+
   return (
     <>
-      <div className="min-h-screen grid grid-cols-1 md:grid-cols-2  justify-center items-center bg-cyan-950  px-4 md:px-0">
+      <ToastContainer />
+      <div className="min-h-screen grid grid-cols-1 md:grid-cols-2 justify-center items-center bg-cyan-950 px-4 md:px-0">
         <div className="w-full max-w-md mx-auto md:ml-32">
-          <form onSubmit={handleLogin} className="bg-gray-100 shadow-md rounded-3xl px-8 md:px-14 py-10 mb-8 ">
+          <form onSubmit={handleLogin} className="bg-gray-100 shadow-md rounded-3xl px-8 md:px-14 py-10 mb-8">
             <h2 className="text-2xl font-bold text-center mb-6 text-indigo-950">Inicio de Sesión</h2>
             <div className="mb-4">
               <label className="block text-indigo-950 text-sm font-bold mb-2" htmlFor="email">Correo</label>
@@ -54,22 +56,20 @@ function Login() {
                 id="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="shadow appearance-none border border-gray-300 rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-4 focus:ring-blue-500"
+                className="shadow-md appearance-none border border-gray-300 rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-4 focus:ring-blue-500"
                 required
               />
-
             </div>
             <div className="mb-6">
-              <label className="block text-indigo-950 text-sm font-bold mb-2" htmlFor="password">Password</label>
+              <label className="block text-indigo-950 text-sm font-bold mb-2" htmlFor="password">Contraseña</label>
               <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="shadow-md appearance-none border border-gray-300 rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-4 focus:ring-blue-500"
-              required
-            />
-
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="shadow-md appearance-none border border-gray-300 rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-4 focus:ring-blue-500"
+                required
+              />
             </div>
             <div className="flex items-center justify-center">
               <button 
@@ -85,20 +85,15 @@ function Login() {
               <Link to="/register" className="text-emerald-400 font-bold hover:text-emerald-500">
                 Regístrate aquí
               </Link>
-           
             </p>
             <Link to="/" className="text-emerald-400 font-bold hover:text-emerald-500">
-             Ir al menú principal
-              </Link>
+              Ir al menú principal
+            </Link>
           </div>
-        </div>
-        <div className="hidden lg:block w-full h-full ">
-          <img src="../assets/fondo.png" alt="Login" loading="Lazy" className="w-full h-full" />
         </div>
       </div>
     </>
   );
 }
-
 
 export default Login;
