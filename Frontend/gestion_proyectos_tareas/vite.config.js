7df189de7830a718e0b5537a5bdc007e37bd3a54
Yerco <yerco.andres.soto@gmail.com>
2024-12-04 11:24:27 -0300

    update login

--- Frontend/gestion_proyectos_tareas/vite.config.js
+++ Frontend/gestion_proyectos_tareas/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': process.env
+  build: {
+    outDir: 'dist'
   }
 });
